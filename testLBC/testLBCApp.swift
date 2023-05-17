410c818ad1e5154fc01a29d8d9ffb0c41c4d130d
Loic D <loic.danj@gmail.com>
2023-05-17 11:36:15 +0200

    Initial Commit

---
+++ testLBC/testLBCApp.swift
@@ -0,0 +1,17 @@
+//
+//  testLBCApp.swift
+//  testLBC
+//
+//  Created by Loic D on 17/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct testLBCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
